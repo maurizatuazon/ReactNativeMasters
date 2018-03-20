c48f0bed489f817821a94a7b73ab95dc3e1a8dad
Mau Tuazon <MauT@Magenic.com>
2018-03-21 00:14:37 +0800

    Initial commit for Week 5 Assignment

--- myproject/android/app/src/main/java/com/myproject/MainApplication.java
+++ myproject/android/app/src/main/java/com/myproject/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import org.pgsqlite.SQLitePluginPackage;
+import org.reactnative.camera.RNCameraPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new SQLitePluginPackage(),
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNCameraPackage()
       );
     }
 
--- myproject/app/screens/Welcome.js
+++ myproject/app/screens/Welcome.js
@@ -4,66 +4,84 @@
  * @flow
  */
 
-import React from 'react';
-import { Button, FlatList, ActivityIndicator, Text, View, AsyncStorage  } from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import Swiper from 'react-native-swiper';
 
-import LaunchService from "../services/LaunchService";
-
-export default class FetchExample extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state ={ isLoading: true}
-  }
-
-  componentDidMount(){
-    this.loadData();
-  }
-
-  async loadData() {
-    var launchData = await LaunchService.getLaunchesAsync();
-    this.setState({
-        isLoading: false,
-        dataSource: launchData
-    });
-  }
-
-  async loadCacheData() {
-      var launchData = await LaunchService.getCachedLaunchesAsync();
-      this.setState({
-          isLoading: false,
-          dataSource: launchData
-      });
-  }
-
-  render(){
-    
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
-
-    return(
-      <View style={{flex: 1, paddingTop:20}}>
-        <Button title="Load from Cache" onPress={async () => {
-                    await this.loadCacheData();
-                }}>
-        </Button>
-        <Button title="Refresh list" onPress={async () => {
-              this.setState({
-                dataSource: JSON.parse('[]')
-              });
-          }}>
-        </Button>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Text>{item.name}, {item.windowstart}, {item.windowend}</Text>}
-          keyExtractor={(item, index) => index}
-        />
+export default class CameraApp extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Swiper style={styles.wrapper} showsButtons={true}>
+          <View style={styles.slide1}>
+            <RNCamera
+                ref={ref => {
+                  this.camera = ref;
+                }}
+                style = {styles.preview}
+                type={RNCamera.Constants.Type.back}
+                flashMode={RNCamera.Constants.FlashMode.on}
+                permissionDialogTitle={'Permission to use camera'}
+                permissionDialogMessage={'We need your permission to use your camera phone'}
+            />
+            <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+              <TouchableOpacity
+                  onPress={this.takePicture.bind(this)}
+                  style = {styles.capture}
+              >
+                  <Text style={{fontSize: 14}}> SNAP </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Beautiful</Text>
+          </View>
+        </Swiper>  
       </View>
     );
   }
+
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options)
+      console.log(data.uri);
+    }
+  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+});
